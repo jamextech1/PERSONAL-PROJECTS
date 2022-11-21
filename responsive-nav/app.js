c2cd3db8d7bfaa70d17a091f78f47f038436bfc2
jamextech1 <okorojameschizaram@gmail.com>
2022-11-21 08:21:52 +0100

    added some corection (changes) to the direction at which the nav move in and out on clicking it

--- responsive-nav/app.js
+++ responsive-nav/app.js
@@ -3,11 +3,11 @@ const btn2 = document.querySelector(".hamburger-close");
 const btn3 = document.querySelector(".nav-links");
 btn.addEventListener("click", function (e) {
   btn2.style.display = "block";
-  btn3.style.top = "7.9%";
+  btn3.style.left = "0";
   btn.style.display = "none";
 });
 btn2.addEventListener("click", function (e) {
   btn2.style.display = "none";
-  btn3.style.top = "-200%";
+  btn3.style.left = "-200%";
   btn.style.display = "block";
 });
