0aae340f4542fb903c411afcdc3fd2b9344a5e71
jamextech1 <okorojameschizaram@gmail.com>
2022-12-12 19:47:51 +0100

    alright

--- responsive-nav/app.js
+++ responsive-nav/app.js
@@ -1,13 +1,6 @@
-const btn = document.querySelector(".hamburger");
-const btn2 = document.querySelector(".hamburger-close");
+const btn = document.querySelector(".toggler");
 const btn3 = document.querySelector(".nav-links");
-btn.addEventListener("click", function (e) {
-  btn2.style.display = "block";
-  btn3.style.left = "0";
-  btn.style.display = "none";
-});
-btn2.addEventListener("click", function (e) {
-  btn2.style.display = "none";
-  btn3.style.left = "-200%";
-  btn.style.display = "block";
+btn.addEventListener("click", function () {
+  btn.classList.toggle("toggle");
+  btn3.classList.toggle("toggle");
 });
